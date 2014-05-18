b627e98149b54570e382f879f425add99ff2d883
fabiohenriquehsp <fabiohenrique.hsp@hotmail.com>
2014-05-18 15:30:36 -0300

    Batalha Naval

--- BatalhaNaval/src/com/senac/mvc/controller/Tabuleiro.java
+++ BatalhaNaval/src/com/senac/mvc/controller/Tabuleiro.java
@@ -1,11 +1,99 @@
 package com.senac.mvc.controller;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class Tabuleiro {
 
-	public static void inicializaTabuleiro(int[][] tabuleiro) {
-		for (int linha = 0; linha < 10; linha++)
-			for (int coluna = 0; coluna < 10; coluna++)
-				tabuleiro[linha][coluna] = -1;
-	}
+	 public void inicializaTabuleiro(int[][] tabuleiro){
+	        for(int linha=0 ; linha < 10 ; linha++ )
+	            for(int coluna=0 ; coluna < 10 ; coluna++ )
+	                tabuleiro[linha][coluna]=-1;
+	    }
+	    
+	    public void mostraTabuleiro(int[][] tabuleiro){
+	        System.out.println("\t1 \t2 \t3 \t4 \t5 \t6 \t7 \t8 \t9 \t10");
+	        System.out.println();
+	        
+	        for(int linha=0 ; linha < 10 ; linha++ ){
+	            System.out.print((linha+1)+"");
+	            for(int coluna=0 ; coluna < 10 ; coluna++ ){
+	                if(tabuleiro[linha][coluna]==-1){
+	                    System.out.print("\t"+".");
+	                }else if(tabuleiro[linha][coluna]==0){
+	                    System.out.print("\t"+"-");
+	                }else if(tabuleiro[linha][coluna]==1){
+	                    System.out.print("\t"+"O");
+	                }
+	                
+	            }
+	            System.out.println();
+	        }
+
+	    }
+
+	    public void iniciaNavios(int[][] navios){
+	        Random sorteio = new Random();
+	        
+	        for(int navio=0 ; navio < 3 ; navio++){
+	            navios[navio][0]=sorteio.nextInt(5);
+	            navios[navio][1]=sorteio.nextInt(5);
+	            
+	            for(int anterior=0 ; anterior < navio ; anterior++){
+	                if( (navios[navio][0] == navios[anterior][0])&&(navios[navio][1] == navios[anterior][1]) )
+	                    do{
+	                        navios[navio][0]=sorteio.nextInt(5);
+	                        navios[navio][1]=sorteio.nextInt(5);
+	                    }while( (navios[navio][0] == navios[anterior][0])&&(navios[navio][1] == navios[anterior][1]) );
+	            }
+	            
+	        }
+	    }
+
+	    public void darTiro(int[] tiro){
+	        Scanner entrada = new Scanner(System.in);
+	        
+	        System.out.print("Linha: ");
+	        tiro[0] = entrada.nextInt();
+	        tiro[0]--;
+	        
+	        System.out.print("Coluna: ");
+	        tiro[1] = entrada.nextInt();
+	        tiro[1]--;
+	        
+	    }
+	    
+	    public boolean acertou(int[] tiro, int[][] navios){
+	        
+	        for(int navio=0 ; navio<navios.length ; navio++){
+	            if( tiro[0]==navios[navio][0] && tiro[1]==navios[navio][1]){
+	                System.out.printf("Você acertou o tiro (%d,%d)\n",tiro[0]+1,tiro[1]+1);
+	                return true;
+	            }
+	        }
+	        return false;
+	    }
+
+	    public void instrucao(int[] tiro, int[][] navios, int tentativa){
+	        int linha=0,
+	            coluna=0;
+	        
+	        for(int fila=0 ; fila < navios.length ; fila++){
+	            if(navios[fila][0]==tiro[0])
+	                linha++;
+	            if(navios[fila][1]==tiro[1])
+	                coluna++;
+	        }
+	        
+	        System.out.printf("\nDica %d: \nlinha %d -> %d navios\n" +
+	                                 "coluna %d -> %d navios\n",tentativa,tiro[0]+1,linha,tiro[1]+1,coluna);
+	    }
+
+	    public void alteraTabuleiro(int[] tiro, int[][] navios, int[][] tabuleiro){
+	        if(acertou(tiro,navios))
+	            tabuleiro[tiro[0]][tiro[1]]=1;
+	        else
+	            tabuleiro[tiro[0]][tiro[1]]=0;
+	    }
 
 }
--- BatalhaNaval/src/com/senac/mvc/view/viewTabuleiro.java
+++ BatalhaNaval/src/com/senac/mvc/view/viewTabuleiro.java
@@ -1,51 +1,42 @@
 package com.senac.mvc.view;
 
+import com.senac.mvc.controller.Tabuleiro;
+
 public class viewTabuleiro {
 
-	public static void mostraTabuleiro(int[][] tabuleiro) {
-		System.out.println("\t0 \t1 \t2 \t3 \t4\t5 \t6 \t7 \t8 \t9");
-		System.out.println();
-		String campos[] = new String[10];
-		campos[0] = "A";
-		campos[1] = "B";
-		campos[2] = "C";
-		campos[3] = "D";
-		campos[4] = "E";
-		campos[5] = "F";
-		campos[6] = "G";
-		campos[7] = "H";
-		campos[8] = "I";
-		campos[9] = "J";
+	 public static void main(String[] args) {
+		 
+		    Tabuleiro t = new Tabuleiro();
+	        int[][] tabuleiro = new int[10][10];
+	        int[][] navios = new int[5][2];
+	        int[] tiro = new int[2];
+	        int tentativas=0,
+	            acertos=0;
+	        
+	        t.inicializaTabuleiro(tabuleiro);
+	        t.iniciaNavios(navios);
+	        
+	        System.out.println();
+	        
+	        do{
+	            t.mostraTabuleiro(tabuleiro);
+	            t.darTiro(tiro);
+	            tentativas++;
+	            
+	            if(t.acertou(tiro,navios)){
+	            	t.instrucao(tiro,navios,tentativas);
+	                acertos++;
+	            }                
+	            else
+	            	t.instrucao(tiro,navios,tentativas);
+	            
+	            t.alteraTabuleiro(tiro,navios,tabuleiro);
+	            
 
-		for (int linha = 0; linha < 10; linha++) {
-			System.out.print((campos[linha]) + "");
-			for (int coluna = 0; coluna < 10; coluna++) {
-				if (tabuleiro[linha][coluna] == -1) {
-					System.out.print("\t" + ".");
-				} else if (tabuleiro[linha][coluna] == 0) {
-					System.out.print("\t" + "-");
-				} else if (tabuleiro[linha][coluna] == 1) {
-					System.out.print("\t" + "0");
-				}
-
-			}
-			System.out.println();
-		}
-	}
-
-	public static void LocalizacaoJogo(int[] tiro, int[][] navios, int tentativa) {
-		int linha = 0, coluna = 0;
-
-		for (int fila = 0; fila < navios.length; fila++) {
-			if (navios[fila][0] == tiro[0])
-				linha++;
-			if (navios[fila][1] == tiro[1])
-				coluna++;
-		}
-
-		System.out.printf("\nLocalizaçao %d: \nlinha %d -> %d navios\n"
-				+ "coluna %d -> %d navios\n", tentativa, tiro[0] + 1, linha,
-				tiro[1] + 1, coluna);
-	}
+	        }while(acertos!=5);
+	        
+	        System.out.println("\n\n\nJogo terminado. Você acertou os 3 navios em "+tentativas+" tentativas");
+	        t.mostraTabuleiro(tabuleiro);
+	    }
 
 }
